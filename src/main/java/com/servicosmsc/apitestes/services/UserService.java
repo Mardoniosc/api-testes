321777f000a34da2c680eda06a07d6ce36544121
mardonio.costa <Mardonio@live.com>
2022-01-19 20:56:00 -0300

    EndPoint FindAll

--- src/main/java/com/servicosmsc/apitestes/services/UserService.java
+++ src/main/java/com/servicosmsc/apitestes/services/UserService.java
@@ -1,8 +1,12 @@
 package com.servicosmsc.apitestes.services;
 
+import java.util.List;
+
 import com.servicosmsc.apitestes.domain.User;
 
 public interface UserService {
 
 	User findById(Integer id);
+	
+	List<User> findAll();
 }
