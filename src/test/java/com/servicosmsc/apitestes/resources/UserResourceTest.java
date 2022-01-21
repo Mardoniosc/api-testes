23d7e2529bc58e82cd08d3adda8adbea5a7d9b00
mardonio.costa <Mardonio@live.com>
2022-01-20 21:37:13 -0300

    Teste UserResource findById com sucesso

--- src/test/java/com/servicosmsc/apitestes/resources/UserResourceTest.java
+++ src/test/java/com/servicosmsc/apitestes/resources/UserResourceTest.java
@@ -1,7 +1,13 @@
 package com.servicosmsc.apitestes.resources;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -9,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import com.servicosmsc.apitestes.domain.User;
 import com.servicosmsc.apitestes.domain.dto.UserDTO;
@@ -41,8 +48,22 @@ class UserResourceTest {
 	}
 
 	@Test
-	void testFindById() {
-		fail("Not yet implemented");
+	void quandoBuscarPorIdRetornaSucesso() {
+		when(service.findById(anyInt())).thenReturn(user);
+		when(mapper.map(any(), any())).thenReturn(userDTO);
+		
+		ResponseEntity<UserDTO> response = resource.findById(ID);
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(ResponseEntity.class ,response.getClass());
+		assertEquals(UserDTO.class, response.getBody().getClass());
+		
+		
+		assertEquals(ID, response.getBody().getId());
+		assertEquals(NOME, response.getBody().getNome());
+		assertEquals(EMAIL, response.getBody().getEmail());
+		assertEquals(SENHA, response.getBody().getSenha());
 	}
 
 	@Test
